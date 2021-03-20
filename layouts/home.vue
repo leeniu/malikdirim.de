b4c944adcd6ff0c087fe6eae0a0f10c02e200717
Cappic90 <git@cappic90.de>
2021-03-20 17:41:40 +0100

    fix: #5 scrollbars in firefox

--- layouts/home.vue
+++ layouts/home.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="text-gray-800 antialiased bg-gray-100 font-light relative flex justify-center w-screen min-h-screen">
+  <div class="text-gray-800 antialiased bg-gray-100 font-light relative flex justify-center w-full h-screen">
     <Particles class="absolute z-0 h-full w-full" />
-    <div class="absolute z-10 flex flex-col min-h-full container mx-auto px-4">
+    <div class="absolute z-10 flex flex-col h-full container mx-auto px-4">
       <Header />
       <nuxt class="flex-1" />
       <Footer />
