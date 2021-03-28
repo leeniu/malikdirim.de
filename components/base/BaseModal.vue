dc3339f6e79e740682a88dd3d7faeb22ee39f3e4
Cappic90 <git@cappic90.de>
2021-03-28 10:56:50 +0200

    GH-13: add base modal

--- components/base/BaseModal.vue
+++ components/base/BaseModal.vue
@@ -1,5 +1,10 @@
 <script>
+import SVGCross from '~/components/svg/SVGCross'
+
 export default {
+  components: {
+    SVGCross
+  },
   methods: {
     close() {
       this.$emit('close')
@@ -9,23 +14,38 @@ export default {
 </script>
 
 <template>
-  <transition name="modal-fade fixed top-0 right-0 bottom-0 left-0 bg-blacke flex justify-center items-center">
-    <div class="modal-backdrop" @click.self="close">
-      <div role="dialog" aria-labelledby="modalTitle" aria-describedby="modalDescription">
-        <header>
-          <slot name="header"> Title </slot>
-          <button type="button" aria-label="Close modal" @click="close">x</button>
-        </header>
-
-        <section>
-          <slot name="body">Body</slot>
-        </section>
-
-        <footer>
-          <slot name="footer">Footer</slot>
-          <button type="button" aria-label="Close modal" @click="close">Close me!</button>
-        </footer>
+  <div class="fixed z-50 inset-0 overflow-y-auto" aria-labelledby="modal-title" role="dialog" aria-modal="true">
+    <div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+      <div
+        class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"
+        style="backdrop-filter: blur(8px)"
+        aria-hidden="true"
+        @click.self="close"
+      ></div>
+      <!-- This element is to trick the browser into centering the modal contents. -->
+      <span class="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
+      <div
+        class="relativ inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full"
+      >
+        <button
+          type="button"
+          class="absolute top-0 right-0 m-4 p-2 h-8 w-8 flex justify-center rounded-lg items-center hover:text-red-600 focus:text-red-600 focus:outline-none"
+          aria-label="Close modal"
+          @click="close"
+        >
+          <SVGCross />
+        </button>
+        <slot>
+          <div class="px-6 py-8 sm:px-8 sm:py-10">
+            <header id="modal-title" class="text-lg leading-6 font-medium text-gray-900">
+              <slot name="header">Title</slot>
+            </header>
+            <section id="modal-description">
+              <slot name="body">Body</slot>
+            </section>
+          </div>
+        </slot>
       </div>
     </div>
-  </transition>
+  </div>
 </template>
