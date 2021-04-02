e36b2a4938a0b5a61f5285f0429541eda981b4ff
Cappic90 <git@cappic90.de>
2021-04-02 09:18:47 +0200

    chore: move script tag to the top of the file

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,21 +1,5 @@
-<template>
-  <div class="flex flex-col justify-center items-center">
-    <NotFound v-if="statusCode === 404" />
-    <div v-else>
-      <svg xmlns="http://www.w3.org/2000/svg" width="90" height="90" fill="#DBE1EC" viewBox="0 0 48 48">
-        <path
-          d="M22 30h4v4h-4zm0-16h4v12h-4zm1.99-10C12.94 4 4 12.95 4 24s8.94 20 19.99 20S44 35.05 44 24 35.04 4 23.99 4zM24 40c-8.84 0-16-7.16-16-16S15.16 8 24 8s16 7.16 16 16-7.16 16-16 16z"
-        />
-      </svg>
-      <div class="title text-xl mb-4">
-        {{ message }}
-      </div>
-    </div>
-  </div>
-</template>
 <script>
 export default {
-  name: 'Error',
   props: {
     error: {
       type: Object,
@@ -37,3 +21,19 @@ export default {
   }
 }
 </script>
+
+<template>
+  <div class="flex flex-col justify-center items-center">
+    <NotFound v-if="statusCode === 404" />
+    <div v-else>
+      <svg xmlns="http://www.w3.org/2000/svg" width="90" height="90" fill="#DBE1EC" viewBox="0 0 48 48">
+        <path
+          d="M22 30h4v4h-4zm0-16h4v12h-4zm1.99-10C12.94 4 4 12.95 4 24s8.94 20 19.99 20S44 35.05 44 24 35.04 4 23.99 4zM24 40c-8.84 0-16-7.16-16-16S15.16 8 24 8s16 7.16 16 16-7.16 16-16 16z"
+        />
+      </svg>
+      <div class="title text-xl mb-4">
+        {{ message }}
+      </div>
+    </div>
+  </div>
+</template>
