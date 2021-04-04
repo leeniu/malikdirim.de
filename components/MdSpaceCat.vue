5ead79666796dabd280396d06785c94a333bfc4f
Cappic90 <git@cappic90.de>
2021-04-04 11:46:34 +0200

    GH-52: fix space for ios

--- components/MdSpaceCat.vue
+++ components/MdSpaceCat.vue
@@ -6,17 +6,21 @@ export default {
 
 <template>
   <div
-    class="h-48 w-48 sm:h-64 sm:w-64 md:h-80 md:w-80 lg:h-96 lg:w-96 relative overflow-hidden rounded-full bg-gradient-to-b from-black to-indigo-900 flex justify-center items-center"
+    class="h-48 w-48 sm:h-64 sm:w-64 md:h-80 md:w-80 lg:h-96 lg:w-96 relative overflow-hidden rounded-full bg-gradient-to-b from-black to-indigo-900 flex justify-center items-center space"
   >
     <span v-for="(star, index) in 75" :key="index" class="bg-yellow-50 rounded-full absolute top-0 left-0 star"></span>
     <span
-      class="block absolute z-10 h-12 w-12 sm:h-16 sm:w-16 md:h-24 md:w-24 animate-fly-around mx-auto"
+      class="block h-12 w-12 sm:h-16 sm:w-16 md:h-24 md:w-24 animate-fly-around mx-auto"
       v-html="require(`~/assets/img/space-kitty.svg?include`)"
     ></span>
   </div>
 </template>
 
 <style lang="scss" scoped>
+.space {
+  -webkit-mask-image: -webkit-radial-gradient(white, black);
+}
+
 @keyframes blinking-star {
   50% {
     transform: scale(0.5);
